579df12239b7e9c2a36efb8b60ff21da72cd55af
Dominic Victoria <hunkyminik@gmail.com>
2021-07-28 12:56:50 -0700

    code 2 started

--- code-v1/src/index.js
+++ code-v1/src/index.js
@@ -1,9 +1,10 @@
 // import _ from 'lodash';
 
+import eat from './print.js';
+
 function isString(data) {
   return data instanceof String || typeof data === 'string';
 }
-
 class SuperDom {
   constructor(element) {
     this.e = element;
@@ -17,16 +18,29 @@ class SuperDom {
 
     return this;
   }
+
+  onClick(task) {
+    this.e.addEventListener('click', task);
+
+    return this;
+  }
 }
 
 const dom = {
   div(...children) {
     return new SuperDom(document.createElement('div')).child(...children);
   },
+  button(...children) {
+    return new SuperDom(document.createElement('button')).child(...children);
+  },
 };
 
 const body = new SuperDom(document.getElementsByTagName('body')[0]);
 
-const app = dom.div('hello there');
+const button = dom.button('Click Me').onClick((e) => {
+  eat();
+});
 
-body.child('hi gago', app);
+const app = dom.div('hello there ');
+
+body.child('hi gago', app, button);
---
+++ code-v1/src/print.js
@@ -0,0 +1,3 @@
+export default function eat() {
+  console.log('Come and eat me Ulo mo');
+}
