0fd7a0e7c431e6869b9a18cba91f0af045dfc4d0
Dominic Victoria <hunkyminik@gmail.com>
2021-07-28 03:28:32 -0700

    clean slate

--- code/src/index.js
+++ code/src/index.js
@@ -1,22 +1,4 @@
 // import _ from 'lodash';
-import './style.css';
-import { shoot } from '../common/shoot';
-import Icon from './arrow.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import Nick from './nick.json';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function isString(data) {
   return data instanceof String || typeof data === 'string';
@@ -47,13 +29,4 @@ const body = new SuperDom(document.getElementsByTagName('body')[0]);
 
 const app = dom.div('hello there');
 
-app.e.className = 'hello';
-app.e.style.height = '500px';
-
 body.child('hi gago', app);
-shoot();
-shoot();
-console.log(Icon);
-console.log(Data);
-console.log(Notes);
-console.log(Nick);
