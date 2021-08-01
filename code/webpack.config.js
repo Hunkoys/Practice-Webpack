4949d19f0f9e42c6144cdfd26c8bc997fb57349d
Dominic Victoria <hunkyminik@gmail.com>
2021-08-01 16:48:04 -0700

    dynamic config for build modes

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,35 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    open: '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'My Webpack',
-    }),
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+module.exports = (env) => {
+  const B = env.WEBPACK_BUILD;
+  const S = env.WEBPACK_SERVE;
+
+  return {
+    mode: B ? 'production' : 'development',
+    entry: './src/index.js',
+    ...(S && { devtool: 'eval-source-map' }),
+    devServer: {
+      contentBase: './dist',
+      open: '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'My Webpack',
+      }),
+    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
+  };
 };
