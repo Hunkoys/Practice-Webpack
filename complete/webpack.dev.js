43108adbfd43004e1b58762e0bd5d157f1f49c25
Dominic Victoria <hunkyminik@gmail.com>
2021-08-07 18:22:35 -0700

    Single project setting

---
+++ complete/webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common.js');
+const project = require('./project.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '(Served) ' + project.title,
+    }),
+  ],
+});
---
+++ complete/webpack.prod.js
@@ -0,0 +1,15 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common.js');
+const project = require('./project.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: project.title,
+    }),
+  ],
+});
