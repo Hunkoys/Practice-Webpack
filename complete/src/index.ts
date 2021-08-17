2151e176f347336cf71e579e0c049b8ab96f8a92
Dominic Victoria <hunkyminik@gmail.com>
2021-08-17 14:40:52 -0700

    Commented some lines

--- complete/src/index.ts
+++ complete/src/index.ts
@@ -1,11 +1,14 @@
-import app from './index/app';
+
 import './style.scss'
 import Show from 'Utilities/show';
 
 const main = document.getElementById('main');
 
+import('./index/app').then(({ default: app }) => {
+  main.append(app);
+  
+})
 
-main.append(app);
 
 Show('Hi sei')
 
--- complete/src/index/app.ts
+++ complete/src/index/app.ts
@@ -1,6 +1,6 @@
-import Felicia from './images/pictures/Felicia.png';
-import iFelicia from './images/pictures/-i-Felicia.png';
-import fFelicia from './images/-i-pictures/Felicia.png';
+// import Felicia from './images/pictures/Felicia.png';
+// import iFelicia from './images/pictures/-i-Felicia.png';
+// import fFelicia from './images/-i-pictures/Felicia.png';
 import extFelicia from 'Img/Felicia.png';
 
 import './app.scss';
@@ -15,9 +15,9 @@ function img(src: string, alt: string) {
   return elem;
 }
 
-const inlineFelicia = img(iFelicia, 'inline');
-const resourceFelicia = img(Felicia, 'resource')
-const folderFelicia = img(fFelicia, 'resource')
+// const inlineFelicia = img(iFelicia, 'inline');
+// const resourceFelicia = img(Felicia, 'resource')
+// const folderFelicia = img(fFelicia, 'resource')
 const externalFelicia = img(extFelicia, 'resource')
 
 App.append( externalFelicia);
