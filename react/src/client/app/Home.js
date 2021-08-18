e5a68536d2857b75073a6296da62422a91b8003a
Dominic Victoria <hunkyminik@gmail.com>
2021-08-18 08:36:40 -0700

    Clean Slate

--- react/src/client/app/Home.js
+++
@@ -1,8 +0,0 @@
-import dom from 'Utilities/dom';
-import './Home.scss';
-
-const heading = dom.div({ id: 'heading' }, 'Hi there');
-
-const hero = dom.div({ id: 'hero' }, heading);
-
-export default hero;
--- react/src/client/index.js
+++
@@ -1,6 +0,0 @@
-import { SuperDom } from 'Utilities/dom';
-import Home from './app/Home';
-
-const main = new SuperDom(document.querySelector('#main'));
-
-main.append(Home);
